ff7248e0612f9f2e53525fde41de536b22d624b7
Jakob Melnyk <jakobmelnyk@hotmail.com>
2011-12-10 19:11:59 +0100

    Started creating the gamestate in code

---
+++ BDSADominion/BDSADominion/Gamestate/Card_Types/Cards/Action_Cards/Attack_Cards/Militia.cs
@@ -0,0 +1,9 @@
+﻿namespace BDSADominion.Gamestate.Card_Types.Cards.Action_Cards.Attack_Cards
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Militia : ActionAttack
+    {
+    }
+}
