857d25376348dd601579e20899eb76efc074a30d
Christian Jensen <Oragada@gmail.com>
2011-12-11 18:30:03 +0100

    Networking added. More or less direct copy of work done outside of project. Interfacing with Control added, but not completed

---
+++ BDSADominion/BDSADominion/Networking/Client.cs
@@ -0,0 +1,103 @@
+﻿namespace BDSADominion.Networking
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    /// <summary>
+    /// This class is a client that can be used to connect to a server,
+    /// to facilitate communication over a network. The communication must be strings.
+    /// </summary>
+    internal class Client
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
+        public Client(IPAddress ipAddress)
+        {
+            ////IPAddress ipAddress = null;
+            ////bool parseSuccess = false;
+
+            ////Console.WriteLine("Please input IP for server:");
+
+            /*while (!parseSuccess)
+            {
+                parseSuccess = IPAddress.TryParse(ip, out ipAddress);
+                if (!parseSuccess)
+                {
+                    Console.WriteLine("ip not valid, try again:");
+                }
+            }*/
+
+            IPEndPoint ipEnd = new IPEndPoint(ipAddress, NetworkConst.PORT);
+
+            Comm = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Comm.Connect(ipEnd);
+
+            BeginReceive();
+
+            ////Comm.BeginReceive(buffer, 0, buffer.length, 0, new AsyncCallback(AcceptReceive), Comm);
+        }
+
+        public event ClientMessageHandler NewMessageEvent;
+
+        /// <summary>
+        /// Gets the Socket through which communication takes place.
+        /// </summary>
+        public Socket Comm { get; private set; }
+
+        private byte[] buffer = new byte[NetworkConst.BUFFERSIZE];
+
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        /// <summary>
+        /// This method should be called whenever the client recieves a message from the server.
+        /// </summary>
+        /// <param name="senderId">
+        /// The network Id of the player responisble for sending the message in the first place.
+        /// </param>
+        /// <param name="message">
+        /// The recieved message
+        /// </param>
+        public void RecievedMessage(string message)
+        {
+            string[] messageParts = message.Split(new char[] { '|' });
+            Console.WriteLine("Client received '{0}' from player {1}", messageParts[1], messageParts[0]);
+        }
+
+        public void BeginReceive()
+        {
+            Comm.BeginReceive(buffer, 0, NetworkConst.BUFFERSIZE, 0, BeginReceiveCallback, this);
+        }
+
+        private void BeginReceiveCallback(IAsyncResult asyncResult)
+        {
+            Console.WriteLine("Client recieve begun");
+            int read = Comm.EndReceive(asyncResult);
+
+            if (read > 0)
+            {
+                stringBuilder.Append(NetworkConst.ENCODER.GetString(buffer, 0, read));
+
+                string content = stringBuilder.ToString();
+
+                ////Console.WriteLine(content);
+
+                if (content.IndexOf("<EOF>") >= 0)
+                {
+                    string message = content.Substring(0, content.Length - 5);
+
+                    if (NewMessageEvent != null)
+                    {
+                        NewMessageEvent(message);
+                    }
+
+                    stringBuilder.Clear();
+                }
+
+                BeginReceive();
+            }
+        }
+    }
+}
---
+++ BDSADominion/BDSADominion/Networking/NetworkConst.cs
@@ -0,0 +1,72 @@
+﻿namespace BDSADominion.Networking
+{
+    using System.Text;
+
+    /// <summary>
+    /// This is the delegate used when a client connects to the server
+    /// </summary>
+    /// <param name="connection">
+    /// The Connection through which the client is connected to the server
+    /// </param>
+    /// <returns>
+    /// Whether a client connected correctly //TODO
+    /// </returns>
+    public delegate bool ConnectedClientHandler(Connection connection);
+
+    /// <summary>
+    /// This is the delegate used when a Connection is closed.
+    /// </summary>
+    /// <param name="connection">
+    /// The Connection through which the client was connected to the server
+    /// </param>
+    public delegate void ClosedConnectionHandler(Connection connection);
+
+    /// <summary>
+    /// This is the delegate used when the server receives a message
+    /// </summary>
+    /// <param name="connection">
+    /// The Connection through which the client is connected to the server
+    /// </param>
+    /// <param name="message">
+    /// The message that has been received
+    /// </param>
+    public delegate void ServerMessageHandler(Connection connection, string message);
+
+    /// <summary>
+    /// This is the delegate used when a client recieves a message
+    /// </summary>
+    /// <param name="message">
+    /// The message that has been recieved
+    /// </param>
+    public delegate void ClientMessageHandler(string message);
+
+    public delegate void InterfaceMessageHandler(string message, int sender);
+
+    /// <summary>
+    /// Contains various System Constants.
+    /// </summary>
+    internal class NetworkConst
+    {
+        /// <summary>
+        /// The encoder used for encrypting and decrypting between strings
+        /// and the byte arrays sent over the network.
+        /// </summary>
+        public static readonly UTF8Encoding ENCODER = new UTF8Encoding();
+
+        /// <summary>
+        /// The standard port used by communications for this network system.
+        /// </summary>
+        public const int PORT = 1337; // Obviously
+
+        /// <summary>
+        /// The standard buffer size for this network system.
+        /// </summary>
+        public const int BUFFERSIZE = 1024;
+    }
+
+    internal enum MessageType
+    {
+        System = 0, Action = 1, Response = 2, WaitResponse = 3
+    }
+
+}
