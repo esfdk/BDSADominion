6d9ef0f3fd5e92063769ce8c18f5d59934845577
Jakob Melnyk <jakobmelnyk@hotmail.com>
2011-12-12 18:26:18 +0100

    Added a todo and want to pull

--- BDSADominion/BDSADominion/Gamestate/Gamestate.cs
+++ BDSADominion/BDSADominion/Gamestate/Gamestate.cs
@@ -217,8 +217,7 @@
             Contract.Requires(Players.Contains(player));
             Contract.Requires(Supply[card] != 0);
 
-            Contract.Ensures();
-
+            // TODO: Ensures Contract.Ensures();
             player.AddCardToZone(CardFactory.CreateCard(card), Zone.Discard);
             Supply[card]--;
         }
